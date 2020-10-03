885af1615e80305d41155c1ae9440e2538b90a42
vladimirtlcik <vladimirtlcik@users.noreply.github.com>
2020-10-03 11:39:40 +0200

    d

---
+++ example.js
@@ -0,0 +1,150 @@
+age
+VM4552:1 Uncaught ReferenceError: age is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM4552:1
+var age = 28
+undefined
+ag2
+VM4619:1 Uncaught ReferenceError: ag2 is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM4619:1
+age
+28
+age = age + 1
+29
+age
+29
+var currentYear = 2020;
+undefined
+currentYear = 2025
+2025
+currentYear - age
+1996
+var favoriteColor = "orange"
+undefined
+favoriteColor
+"orange"
+var isItChristmas = false
+undefined
+var asd = 5
+undefined
+sda
+VM5408:1 Uncaught ReferenceError: sda is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM5408:1
+sad
+VM5417:1 Uncaught ReferenceError: sad is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM5417:1
+addEventListeners
+VM5427:1 Uncaught ReferenceError: addEventListeners is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM5427:1
+dsa
+VM5435:1 Uncaught ReferenceError: dsa is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM5435:1
+sa
+VM5442:1 Uncaught ReferenceError: sa is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM5442:1
+DataTransferdsad
+VM5449:1 Uncaught ReferenceError: DataTransferdsad is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM5449:1
+sa
+VM5454:1 Uncaught ReferenceError: sa is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM5454:1
+s
+VM5462:1 Uncaught ReferenceError: s is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM5462:1
+typeof(asd)
+"number"
+asd
+5
+typeof(favoriteColor)
+"string"
+typeof(isItChristmas)
+"boolean"
+var friend1 = "Joe"
+undefined
+var friend2 = "Bill"
+undefined
+var friend3 = "Francis"
+undefined
+[]
+[]
+["Joe", "Bill", "Francis"]
+(3) ["Joe", "Bill", "Francis"]
+var myFriends = ["Joe", "Bill", "Francis"]
+undefined
+myFriends
+(3) ["Joe", "Bill", "Francis"]
+typeof(myFriends)
+"object"
+myFriends.length
+3
+friend1.length
+3
+asd.length
+undefined
+isItChristmas.length
+undefined
+myFriends[2]
+"Francis"
+myFriends[3]
+undefined
+myFriends[-50]
+undefined
+myFriends[1] = "Jessica"
+"Jessica"
+myFriends
+(3) ["Joe", "Jessica", "Francis"]
+myFriends[0] = myFriends[0] + "y"
+"Joey"
+myFriends
+(3) ["Joey", "Jessica", "Francis"]0: "Joey"1: "Jessica"2: "Francis"length: 3__proto__: Array(0)
+myFriends[3] = "Paul"
+"Paul"
+myFriends
+(4) ["Joey", "Jessica", "Francis", "Paul"]
+myFriends[myFriends.length] = "Johny"
+"Johny"
+myFriends.push("Trump")
+6
+myFriends
+(6) ["Joey", "Jessica", "Francis", "Paul", "Johny", "Trump"]
+myFriends.pop()
+"Trump"
+myFriends
+(5) ["Joey", "Jessica", "Francis", "Paul", "Johny"]
+myFriends.push(["Obama", "Biden"])
+6
+myFriends
+(6) ["Joey", "Jessica", "Francis", "Paul", "Johny", Array(2)]
+myFriends.pop()
+(2) ["Obama", "Biden"]
+myFriends.push("Obama", "Biden")
+7
+myFriends
+(7) ["Joey", "Jessica", "Francis", "Paul", "Johny", "Obama", "Biden"]
+var array = []
+undefined
+array.push([])
+1
+array
+[Array(0)]0: Array(0)length: 0__proto__: Array(0)length: 1__proto__: Array(0)
+array[0] = [1, 2]
+(2) [1, 2]
+array
+[Array(2)]0: (2) [1, 2]length: 1__proto__: Array(0)
+array.push([3,4,5])
+2
+array
+(2) [Array(2), Array(3)]0: (2) [1, 2]1: (3) [3, 4, 5]length: 2__proto__: Array(0)
+var array2 = [1, "apple", true, [1, 2]]
+undefined
+array2
+(4) [1, "apple", true, Array(2)]
